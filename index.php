fc70b6baebec20938d0d8604b6d1d157b2cd98c9
BE Design <info@b-ed.com>
2014-04-16 10:04:09 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title>@iknowbryan | Aim High. Stay Fresh. Be Creative.</title>
+		
+		<link rel="stylesheet" href="css/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+		<script type="text/javascript" src="js/instafeed.js"></script>
+		<script type="text/javascript" src="js/twitterfeed.js"></script>
+		<script type="text/javascript" src="js/init.js"></script>
+	</head>
+
+	<body>
+
+		<div id="wrap">
+			<div id="profile-images">
+				<div id="profile-image-0" class="image"></div>
+				<div id="profile-image-1" class="image"></div>
+				<div id="profile-image-2" class="image"></div>
+			</div>
+			<div id="header">
+				<h1>@iknowbryan</h1>
+				<div class="social-icons block-btns">
+					<a href="http://twitter.com/iknowbryan" target="_blank" class="icon-twitter"></a>
+					<a href="http://facebook.com/iknowbryan" target="_blank" class="icon-facebook"></a>
+					<a href="http://instagram.com/iknowbryan" target="_blank" class="icon-instagram"></a>
+					<a href="http://plus.google.com/BryanEdwards" target="_blank" class="icon-googleplus"></a>
+				</div>
+			</div>
+			<div id="main">
+				
+				<div id="whats-up">
+					<h1>What I've been up to...</h1>
+
+					<div id="instagram">
+						<span class="icon-instagram icon"></span>
+						<div id="instafeed" class="instafeed"></div>
+					</div>
+
+					<div id="tweets">
+						<span class="icon-twitter icon"></span>
+						<div id="tweet-holder"></div>
+					</div>
+
+				</div>
+				
+				<h1>Look what I can do!</h1>
+
+				<div id="be-design" class="project">
+					<div class="header">
+						I can help define your identity.
+					</div>
+					<div class="cover">
+						<a href="http://b-ed.com" target="_blank">BE Design</a>
+					</div>
+					<div class="info">
+						<h1>
+							<a href="http://b-ed.com" target="_blank">BE Design</a>
+						</h1>
+						<div class="tagline">
+							Even the sky has no limits.
+						</div>
+						<div class="desc">
+							We are a creative agency specilizing in website design & development, graphic design solutions, and brand development & consulting.
+						</div>
+						<div class="footer">
+							<div class="site">
+								<a href="http://b-ed.com" target="_blank">www.b-ed.com</a>
+							</div>
+
+							<div class="social-icons block-btns">
+								<a href="http://twitter.com/be_design" target="_blank" class="icon-twitter"></a>
+								<a href="http://facebook.com/bedesignla" target="_blank" class="icon-facebook"></a>
+								<a href="http://instagram.com/be_design" target="_blank" class="icon-instagram"></a>
+								<a href="https://plus.google.com/+BedesignLA" target="_blank" class="icon-googleplus"></a>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+				
+
+				<div id="fc" class="project">
+					<div class="header">
+						I can help you help the world.
+					</div>
+					<div class="cover">
+						<div class="inner">
+							<div class="fc-icons">
+								<a href="http://freshcharities.com/people" class="icon-people"></a>
+								<a href="http://freshcharities.com/enviro" class="icon-enviro"></a>
+								<a href="http://freshcharities.com/animal" class="icon-animal"></a>
+							</div>
+							<div class="title"><a href="http://freshcharities.com" target="_blank"><span>Fresh</span> Charities</a></div>
+							<div class="tag">The Social Network for Social Change.</div>
+						</div>
+						
+					</div>
+					<div class="info">
+						<h1>
+							<a href="http://freshcharities.com" target="_blank">Fresh Charities</a>
+						</h1>
+						<div class="tagline">
+							The Social Network for Social Change.
+						</div>
+						<div class="desc">
+							We bring globlal reach to grassroots organizations and make it easy to find, dontate to, and get involved with amazing charities from around the world.
+						</div>
+						<div class="footer">
+							<div class="site">
+								<a href="http://b-ed.com" target="_blank">www.freshcharities.com</a>
+							</div>
+							<div class="social-icons block-btns">
+								<a href="http://twitter.com/freshcharities" target="_blank" class="icon-twitter"></a>
+								<a href="http://facebook.com/freshcharities" target="_blank" class="icon-facebook"></a>
+								<a href="http://instagram.com/freshcharities" target="_blank" class="icon-instagram"></a>
+								<a href="http://plus.google.com/freshcharities" target="_blank" class="icon-googleplus"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div id="aim-high" class="project">
+					<div class="header">
+						I can post really cool stuff.
+					</div>
+					<div class="cover">
+						<div class="inner">
+							<a href="http://aimhighforlife.com" target="_blank"><img src="images/Aim-High-skyline.jpg" alt="Aim High Skyline" /></a>
+						</div>
+						
+					</div>
+					<div class="info">
+						<h1>
+							<a href="http://aimhighforlife.com" target="_blank">Aim High</a>
+						</h1>
+						<div class="tagline">
+							Lifestay & Design.
+						</div>
+						<div class="desc">
+							Through photography, literature, lifestyle & design we intend to hold the target up in order to inspire people to Aim High.
+						</div>
+						<div class="footer">
+							<div class="site">
+								<a href="http://b-ed.com" target="_blank">www.aimhighforlife.com</a>
+							</div>
+							<div class="social-icons block-btns">
+								<a href="http://twitter.com/iknowbryan" target="_blank" class="icon-twitter"></a>
+								<a href="http://facebook.com/iknowbryan" target="_blank" class="icon-facebook"></a>
+								<a href="http://instagram.com/iknowbryan" target="_blank" class="icon-instagram"></a>
+								<a href="http://plus.google.com/BryanEdwards" target="_blank" class="icon-googleplus"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div id="footer">
+					{ Stay Fresh. }
+				</div>
+
+				<div id="back-to-top">
+					<a href="#" class="icon-arrow-up"></a>
+				</div>
+			</div>
+			
+		</div>
+
+		
+
+	</body>
+
+</html>
